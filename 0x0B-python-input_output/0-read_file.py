8fe53c64d584aa4fa1c93f2cceca53ce4e744d1a
root <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>
2022-02-01 01:34:16 -0800

    task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+'''function that reads a text file 
+(UTF8) and prints it to stdout'''
+
 def read_file(filename=""):
+    '''
+    using with statement
+    i don't need to manage file permission
+    im not allowed to import
+    '''
     with open(filename, "r", encoding="utf-8") as f:
         print(f.read(), end="")
