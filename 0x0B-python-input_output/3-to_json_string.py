2fedcf63ebf57dd9c0bae8edfa93971e6afb4cc1
root <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>
2022-02-01 03:16:45 -0800

    task 3

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 '''function that returns the JSON 
 representation of an object (string)'''
-def to_json_string(my_list):
+import json
+def to_json_string(my_obj):
     '''
     you don’t need to manage exceptions if the object can’t be serialized.
     '''
-    return json.dumps(my_list)
+    return json.dumps(my_obj)
