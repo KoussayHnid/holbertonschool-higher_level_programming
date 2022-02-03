c3b73d40ea5a0bcbf29b6c74626ab93efbab2722
root <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>
2022-02-03 06:46:30 -0800

    task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+''' function that returns the list of available attributes and methods of an objec'''
+def lookup(obj):
+    '''
+    Returns a list object
+    You are not allowed to import any modul
+    '''
+    return (dir(obj))
