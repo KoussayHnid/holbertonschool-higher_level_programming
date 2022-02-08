7bab416397fa574e2f126ebcb4456ba4b2e82f4e
root <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>
2022-02-08 02:54:36 -0800

    ttask 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''
+Write a class Square that defines a square by: (based on 0-square.py)
+'''
+class Square:
+    '''
+    Private instance attribute: size
+    Instantiation with size (no type/value verification)
+    You are not allowed to import any module
+    '''
+    def __init__(self, size):
+        '''
+        The size of a square is crucial for a square
+        '''
+        self.__size = size
