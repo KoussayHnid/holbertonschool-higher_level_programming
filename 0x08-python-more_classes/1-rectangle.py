8da283084fe5568e048a8bdab0502534ff97b04b
root <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>
2022-04-11 03:49:21 -0700

    task 1

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+'''class Rectangle that defines a rectangle by: (based on 0-rectangle.py)'''
+
+
+class Rectangle:
+    '''
+    You are not allowed to import any module
+    '''
+
+    def __init__(self, width=0, height=0):
+        '''
+        Instantiation with optional
+        '''
+         self.width = width
+        self.height = height
+
+        @property
+    def width(self):
+        '''
+        to retrieve it
+        '''
+         return self.__width
+
+     @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+         @property
+    def height(self):
+        '''
+        Private instance attribute
+        '''
+         return self.__heigh
+
+     @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
