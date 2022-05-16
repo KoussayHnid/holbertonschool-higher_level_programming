cbacc1ff7e415f2b283bec21b9ab9bf4204501fa
root <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>
2022-05-16 03:51:39 -0700

    task 2

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const arg = process.agrv;
+const len = arg.length;
+if ( len === 2 )
+{
+	console.log('No argument');
+}
+	else if ( len === 3 ) {
+		 console.log('Argument found');
+	}
+	else{
+		console.log('Argument found');
+}
