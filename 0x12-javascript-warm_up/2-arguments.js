cb2f1cd05be37fa96ad856e8a6319ff7d0cd7fc0
koussay557 <91122556+koussay557@users.noreply.github.com>
2022-05-17 12:04:57 +0200

    Update 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,12 @@
 #!/usr/bin/node
 
 const len = process.argv.length;
-if ( len === 2 ){
+if (len === 2)
+{
 	console.log('No argument');
 }
-else if ( len === 3 ){
+else if (len === 3)
+{
 	console.log('Argument found');
 }
 else{
