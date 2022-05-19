657c2da74b3837d37eb2165f5dccf16c14fc0039
root <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>
2022-05-19 04:59:45 -0700

    task 0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''
+script that list all states from database
+'''
+import MySQLdb
+from sys import argv
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        password=argv[2],
+        database=argv[3],
+
+    )
+    cursor = db.cursor()
+    sql = "SELECT * FROM states ORDER BY id"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    for row in results:
+        print(row)
+        cursor.close()
+        db.close()
