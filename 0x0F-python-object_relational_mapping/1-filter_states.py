2a8a46075dd87b0d89f21feb8b2e94787e7825dc
root <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>
2022-05-20 03:09:37 -0700

    task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -3,11 +3,10 @@
 script that list all states from database
 '''
 
-    import MySQLdb
+import MySQLdb
+from sys import argv
 
-    from sys import argv
-
-   if __name__ == "__main__":
+if __name__ == "__main__":
 
         db = MySQLdb.connect(
             host="localhost",
