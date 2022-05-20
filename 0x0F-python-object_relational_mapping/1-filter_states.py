7c36c4e5602d997667a3752ad63a114c0dade41d
root <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>
2022-05-20 03:11:52 -0700

    task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,7 +8,7 @@ from sys import argv
 
 if __name__ == "__main__":
 
-        db = MySQLdb.connect(
+    db = MySQLdb.connect(
             host="localhost",
             port=3306,
             user=argv[1],
